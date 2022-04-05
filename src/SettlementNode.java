43301de9d704777c9aab6eb69de067f09856840e
slhscs295 <siddhibavare@gmail.com>
2022-04-05 11:49:40 -0500

    methods added

--- src/SettlementNode.java
+++ src/SettlementNode.java
@@ -1,2 +1,11 @@
 public class SettlementNode {
+    public int getPlayer(){
+
+    }
+    public int getDevelopment(){
+
+    }
+    public void setPlayer(int n){}
+
+    public void setDevelopment(int n){}
 }
