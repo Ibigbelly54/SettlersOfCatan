61e0bda9981dc48236306de6b734d35a1aaa039c
h0800 <sugarcake2468@gmail.com>
2022-04-06 12:18:08 -0500

    x

---
+++ src/Player.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Player
+{
+    boolean largestArmy, longestRoad;
+    ArrayList<ResourceCard> playerHand;
+    ArrayList<DevelopmentCard> devCards;
+    int vp, cities, roads, settlements, knight;
+
+    public Player()
+    {
+
+    }
+    public void setVP(int vp) {
+        this.vp = vp;
+    }
+    public int getVP()
+    {
+        return vp;
+    }
+    public void addCard(ResourceCard card)
+    {
+        playerHand.add(card);
+    }
+    public boolean deleteCard(ResourceCard card)
+    {
+        return playerHand.remove(card);
+    }
+    public void setKnight(int num)
+    {
+        knight=num;
+    }
+    public ArrayList<ResourceCard> getHand()
+    {
+        return playerHand;
+    }
+    public ArrayList<DevelopmentCard> getDevCards()
+    {
+        return getDevCards();
+    }
+}
